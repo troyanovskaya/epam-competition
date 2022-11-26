f7e60de65491ec202f43086ccd499eaaca4197ee
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-26 17:53:53 +0000

    refactor: about-us-container with for loop feat: add icon

--- localGoods/src/app/app.module.ts
+++ localGoods/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthPannelComponent } from './components/auth-pannel/auth-pannel.component';
 import { NavPanelComponent } from './components/nav-panel/nav-panel.component';
-import { NavOptionComponent } from './nav-option/nav-option.component';
+import { NavOptionComponent } from './components/nav-option/nav-option.component';
 import { LocalVendorsSearchAdverbComponent } from './components/local-vendors-search-adverb/local-vendors-search-adverb.component';
 import { VendorsSearchComponent } from './components/vendors-search/vendors-search.component';
 import { VendorsAdverbComponent } from './components/vendors-adverb/vendors-adverb.component';
--- localGoods/src/app/components/about-us-container/about-us-container.component.ts
+++ localGoods/src/app/components/about-us-container/about-us-container.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us-container.component.css']
 })
 export class AboutUsContainerComponent implements OnInit {
+  headers: string[] = ['Prices are the same as in the store',
+  'Wide range of products', 'Careful ordering', 'Prompt home delivery'];
+  paragraphs: string[] = ['We offer the same price as the retailer. Moreover, we collect promotional products in one place so that your purchases are profitable.',
+  'The entire range of products on the site is the same as in the market. Of the advantages: no need to wander department store - just press a button, because we have all the products are sorted in categories.',
+  'Our shoppers (people who collect an order for you) choose only the best products from those offered in the store and follow the rules of the commodity neighborhood. We form an order as for ourselves.',
+  'Our logistics department thinks out in detail the courier`s route to your home so that you receive your order on time and without delays.']
 
   constructor() { }
 

28552b72a0884a45e49689b2dbbfff325ac6d974
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-11-27 16:08:23 +0300

    Create database context class

---
+++ LocalGoodsBackend/LocalGoods.DAL/Contexts/LocalGoodsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LocalGoods.DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalGoods.DAL.Contexts
+{
+    public class LocalGoodsDbContext: IdentityDbContext<User, Role, Guid>
+    {
+        public LocalGoodsDbContext(DbContextOptions<LocalGoodsDbContext> options): base(options){}
+    }
+}
--- LocalGoodsBackend/LocalGoods.DAL/Entities/User.cs
+++ LocalGoodsBackend/LocalGoods.DAL/Entities/User.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace LocalGoods.DAL.Entities
 {
     // TODO - Inherit from IdentityUser instead of EntityBase
-    public class User: EntityBase
+    public class User: IdentityUser<Guid>
     {
         public Vendor Vendor { get; set; }
         public Guid VendorId { get; set; }

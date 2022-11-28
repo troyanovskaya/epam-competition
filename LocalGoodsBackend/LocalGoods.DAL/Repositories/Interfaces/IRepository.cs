28c85eaf2218a8e4a3afefaf5d0f068b8c6c233f
NMishchenko <mishchenko.nls@gmail.com>
2022-11-28 15:13:17 +0200

    Add all repository interfaces

---
+++ LocalGoodsBackend/LocalGoods.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LocalGoods.DAL.Repositories.Interfaces
+{
+    public interface IRepository<TId, TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetByFilterAsync(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> GetByIdAsync(TId id);
+        Task AddAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task DeleteByIdAsync(TId id);
+        Task UpdateAsync(TEntity entity);
+        Task<bool> CheckIfEntityExistsById(TId id);
+    }
+}

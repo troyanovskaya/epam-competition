696978477cadea242f3751d2c062e32c63f3f091
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-11-29 21:47:37 +0300

    Add authentication service and jwt handler

---
+++ LocalGoodsBackend/LocalGoods.BLL/Services/AuthService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using AutoMapper;
+using LocalGoods.BLL.Interfaces;
+using LocalGoods.BLL.Models.Auth;
+using LocalGoods.BLL.Models.Auth.JWT;
+using LocalGoods.DAL.Entities;
+using LocalGoods.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace LocalGoods.BLL.Services
+{
+    public class AuthService: IAuthService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly IJwtHandler _jwtHandler;
+        //private readonly ICityRepository _cityRepository;
+
+        public AuthService(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IMapper mapper, 
+            IJwtHandler jwtHandler) 
+            //ICityRepository cityRepository)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _jwtHandler = jwtHandler;
+            //_cityRepository = cityRepository;
+        }
+
+        public async Task SignupAsync(SignupModel model)
+        {
+            // TODO - Uncomment when we have some cities added
+            /*if (!await _cityRepository.CheckIfEntityExistsById(model.CityId))
+            {
+                throw new Exception($"City with id {model.CityId} was not found");
+            }*/
+            
+            var user = _mapper.Map<User>(model);
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception("Duplicate user name or email");
+            }
+
+            // TODO - Seed Roles and add user to a default one
+            //await _userManager.AddToRoleAsync(user, "Visitor");
+        }
+
+        public async Task<JwtResponse> LoginAsync(LoginModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                throw new Exception(model.Email);
+            }
+
+            var result = await _signInManager
+                .PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                throw new InvalidCredentialException("Invalid email or password");
+            }
+
+            var claims = await _jwtHandler.GetClaimsAsync(user);
+            var signingCredentials = _jwtHandler.GetSigningCredentials();
+            var token = _jwtHandler.GenerateToken(signingCredentials, claims);
+
+            return new JwtResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                ValidTo = token.ValidTo
+            };
+        }
+    }
+}
---
+++ LocalGoodsBackend/LocalGoods.BLL/Services/JwtHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using LocalGoods.BLL.Interfaces;
+using LocalGoods.BLL.Models.Auth.JWT;
+using LocalGoods.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LocalGoods.BLL.Services
+{
+    public class JwtHandler: IJwtHandler
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<User> _userManager;
+        
+        public JwtHandler(
+            IOptions<JwtSettings> jwtSettings,
+            UserManager<User> userManager)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _userManager = userManager;
+        }
+        
+        public SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.Key);
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        public async Task<List<Claim>> GetClaimsAsync(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(userRoles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role)));
+
+            return claims;
+        }
+        
+        public JwtSecurityToken GenerateToken(SigningCredentials signingCredentials,
+            IEnumerable<Claim> claims)
+        {
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signingCredentials);
+
+            return token;
+        }
+    }
+}

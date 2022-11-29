b214e411498ee74f79cfc4b277e88c1d6d695b36
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-11-29 21:50:47 +0300

    Add authentication models

---
+++ LocalGoodsBackend/LocalGoods.BLL/Models/Auth/JWT/JwtResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LocalGoods.BLL.Models.Auth.JWT
+{
+    public class JwtResponse
+    {
+        public string Token { get; set; }
+        public DateTime ValidTo { get; set; }
+    }
+}
---
+++ LocalGoodsBackend/LocalGoods.BLL/Models/Auth/JWT/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalGoods.BLL.Models.Auth.JWT
+{
+    public class JwtSettings: IJwtSettings
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}

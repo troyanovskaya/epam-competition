da81f92713c83a5317bbba7d8204e4e18e192670
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-11-29 21:52:12 +0300

    Add all required extensions for Service Collection in BLL

---
+++ LocalGoodsBackend/LocalGoods.BLL/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using LocalGoods.BLL.Interfaces;
+using LocalGoods.BLL.MappingProfiles;
+using LocalGoods.BLL.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LocalGoods.BLL.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBllServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.ConfigureAuthentication(configuration);
+            services.ConfigureServices();
+
+            return services;
+        }
+        
+        private static IServiceCollection ConfigureAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration.GetSection("JwtSettings")["Issuer"],
+                        ValidAudience = configuration.GetSection("JwtSettings")["Audience"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("JwtSettings")["Key"]))
+                    };
+                });
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureServices(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IJwtHandler, JwtHandler>();
+            services.AddScoped<IAuthService, AuthService>();
+
+            return services;
+        }
+    }
+}

ce2d2ce6f1ff831a05416e32a6808cd1f5a3c6bf
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-11-29 21:52:44 +0300

    Add auth models in PL

---
+++ LocalGoodsBackend/LocalGoods.PL/Models/Auth/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalGoods.PL.Models.Auth
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}

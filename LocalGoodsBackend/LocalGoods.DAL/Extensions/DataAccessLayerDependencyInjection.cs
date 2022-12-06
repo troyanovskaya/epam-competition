971c62f2ea090dfadc112f13f4baf7aeac758678
Kostiantyn Bondarenko <kostiantyn_bondarenko1@epam.com>
2022-12-06 03:10:57 +0200

    Add email integration and email confirmation of the accounts

--- LocalGoodsBackend/LocalGoods.DAL/Extensions/DataAccessLayerDependencyInjection.cs
+++ LocalGoodsBackend/LocalGoods.DAL/Extensions/DataAccessLayerDependencyInjection.cs
@@ -42,7 +42,7 @@ namespace LocalGoods.DAL.Extensions
                     opt.Password.RequireNonAlphanumeric = false;
                     opt.User.RequireUniqueEmail = true;
                     // TODO - Set to false when we have email confirmation functionality
-                    opt.SignIn.RequireConfirmedEmail = false;
+                    opt.SignIn.RequireConfirmedEmail = true;
                 })
                 .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<LocalGoodsDbContext>()

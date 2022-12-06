83fa2199092b4e8003149d05da634fdd5e7074e3
NMishchenko <mishchenko.nls@gmail.com>
2022-12-07 00:43:49 +0200

    Fix same identifiers

--- LocalGoodsBackend/LocalGoods.Shared/GlobalValues.cs
+++ LocalGoodsBackend/LocalGoods.Shared/GlobalValues.cs
@@ -5,6 +5,6 @@ namespace LocalGoods.Shared
     public static class GlobalValues
     {
         public static Guid NewOrderStatusId = new Guid("5cd43639-e879-4331-9e3b-019537bb729b");
-        public static Guid CompletedOrderStatusId = new Guid("5cd43639-e879-4331-9e3b-019537bb729b");
+        public static Guid CompletedOrderStatusId = new Guid("17cf0057-aa23-4cdf-96a4-6573c7ae96e6");
     }
 }

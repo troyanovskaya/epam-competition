c08befa96a782d5923d2bbc45232c13b90ada513
NMishchenko <mishchenko.nls@gmail.com>
2022-12-13 02:37:04 +0200

    Add all necessary validators

---
+++ LocalGoodsBackend/LocalGoods.BLL/Validators/User/LoginModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LocalGoods.BLL.Models.Auth;
+
+namespace LocalGoods.BLL.Validators.User
+{
+    public class LoginModelValidator : AbstractValidator<LoginModel>
+    {
+        public LoginModelValidator()
+        {
+            RuleFor(lm => lm.Email).NotEmpty().EmailAddress();
+            RuleFor(lm => lm.Password).NotEmpty().Length(8, 100);
+        }
+    }
+}
---
+++ LocalGoodsBackend/LocalGoods.BLL/Validators/Vendor/CreateVendorModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using LocalGoods.BLL.Models.Vendor;
+using LocalGoods.BLL.Validators.DeliveryInformation;
+using LocalGoods.BLL.Validators.PaymentInformation;
+
+namespace LocalGoods.BLL.Validators.Vendor
+{
+    public class CreateVendorModelValidator : AbstractValidator<CreateVendorModel>
+    {
+        public CreateVendorModelValidator()
+        {
+            RuleFor(cvm => cvm.Name).NotEmpty().Length(5, 50).Matches("^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$");
+            RuleFor(cvm => cvm.ViberNumber).NotEmpty().Length(11, 15).Matches("^[0-9]*$");
+            RuleFor(cvm => cvm.TelegramName).NotEmpty().Length(5, 100);
+            RuleFor(cvm => cvm.InstagramName).NotEmpty().Length(5, 255);
+            RuleFor(cvm => cvm.PaymentMethods).NotNull();
+            RuleForEach(cvm => cvm.PaymentMethods).NotNull().SetValidator(new PaymentInformationModelValidator());
+            RuleFor(cvm => cvm.DeliveryMethods).NotNull();
+            RuleForEach(cvm => cvm.DeliveryMethods).NotNull().SetValidator(new DeliveryInformationModelValidator());
+        }
+    }
+}

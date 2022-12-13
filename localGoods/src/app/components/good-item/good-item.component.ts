10ec617cfd8dbb296216dc1fd717c414e76333b8
Anastasiia <troyanovskaya2003@gmail.com>
2022-12-13 15:44:06 +0000

    feat: add item to basket

--- localGoods/src/app/components/good-item/good-item.component.ts
+++ localGoods/src/app/components/good-item/good-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Good } from 'src/app/schema/good.model';
+import { BasketService } from 'src/app/services/basket.service';
 
 @Component({
   selector: 'app-good-item',
@@ -11,7 +12,18 @@ export class GoodItemComponent implements OnInit {
     price: 0, poster: '', discount: 0, vendorId: '0', amount: 0,  unitType: {
     id: '0', name: ''}, categories: [], images: []};
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
+  addToBasket(){
+    let basketItem  = this.basketService.basket.find( el => el.good.id === this.item.id);
+    if(basketItem){
+      basketItem.quantity = basketItem.quantity+1;
+      this.basketService.basket = this.basketService.basket.filter( el => el.good.id !== this.item.id);
+      this.basketService.basket.push(basketItem);
+    } else{
+      this.basketService.basket.push({good:this.item, quantity:1});
+    }
+    this.basketService.onTotalChange();
+  }
 
   ngOnInit(): void {
   }
--- localGoods/src/app/services/basket.service.ts
+++ localGoods/src/app/services/basket.service.ts
@@ -7,30 +7,7 @@ import { OrderItem } from '../schema/orderItem.model';
   providedIn: 'root'
 })
 export class BasketService {
-  basket:OrderItem[] = [{good:{  id: '1', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 3, poster: 'assets/carrot.jpg', discount: 0, vendorId: '3',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '2', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '1',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '3', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '2',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '4', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '1',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '5', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '1',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '6', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '2',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '7', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 123, poster: 'assets/carrot.jpg', discount: 0, vendorId: '1',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},
-    {good:{  id: '8', name: 'carrot', description: 'Sweet fresh carrot that can benefit your health',
-    price: 3, poster: 'assets/carrot.jpg', discount: 0, vendorId: '3',   amount: 66,  unitType: {
-    id: '0', name: 'kg'}, categories: [], images: ['assets/carrot.jpg']}, quantity:1},].sort((el1, el2) => el1.good.vendorId.localeCompare(el2.good.vendorId));
+  basket:OrderItem[] = [];
 
   showVendor:string = 'all';
   total:number = this.basket.reduce((pastVal, currentEl) => pastVal+currentEl.good.price*currentEl.quantity, 0);

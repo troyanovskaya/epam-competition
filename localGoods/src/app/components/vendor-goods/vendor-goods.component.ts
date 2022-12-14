36b99a583a7110032104811ebcc24306851c4a87
Nune Mazmanyan <mazmanyan2004@gmail.com>
2022-12-14 22:52:39 +0400

    first commit

--- localGoods/src/app/components/vendor-goods/vendor-goods.component.ts
+++ localGoods/src/app/components/vendor-goods/vendor-goods.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { PublishedGoodsService } from 'src/app/services/published-goods.service';
+import { VendorGoodCraeationComponent } from './vendor-good-craeation/vendor-good-craeation.component';
 
 @Component({
   selector: 'app-vendor-goods',
   templateUrl: './vendor-goods.component.html',
   styleUrls: ['./vendor-goods.component.css']
 })
-export class VendorGoodsComponent implements OnInit {
+export class VendorGoodsComponent{
 
-  constructor(public publishedGoodsService: PublishedGoodsService) { }
+  constructor(public publishedGoodsService: PublishedGoodsService,
+    private dialogRef: MatDialog) { }
 
-  ngOnInit(): void {
-  }
+ openCreationPage(){
+  this.dialogRef.open(VendorGoodCraeationComponent, {
+    height: '60%',
+    width: '50%',
+    panelClass: 'custom-dialog-container'
+  },
+  );
+ }
 
 }

b06b2b26f10ee86daffb4789ab6024c3e89a907c
NMishchenko <mishchenko.nls@gmail.com>
2022-12-14 14:07:21 +0200

    Fix phone number length in validators

--- LocalGoodsBackend/LocalGoods.BLL/Validators/Vendor/CreateVendorModelValidator.cs
+++ LocalGoodsBackend/LocalGoods.BLL/Validators/Vendor/CreateVendorModelValidator.cs
@@ -10,7 +10,7 @@ namespace LocalGoods.BLL.Validators.Vendor
         public CreateVendorModelValidator()
         {
             RuleFor(cvm => cvm.Name).NotEmpty().Length(5, 50).Matches("^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$");
-            RuleFor(cvm => cvm.ViberNumber).Length(11, 15).Matches("^\\+[0-9]*$");
+            RuleFor(cvm => cvm.ViberNumber).Length(12, 16).Matches("^\\+[0-9]*$");
             RuleFor(cvm => cvm.TelegramName).Length(5, 100);
             RuleFor(cvm => cvm.InstagramName).Length(5, 255);
             RuleFor(cvm => cvm.PaymentMethods).NotNull();
